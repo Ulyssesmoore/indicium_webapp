14de00a3373a87267ab853744bb137d5a8c19664
DESKTOP-T428JUH\Ulyssesmoore <sanderdebruin5c@gmail.com>
2017-04-03 11:57:25 +0200

    Add project files.

---
+++ indicium_webapp/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace indicium_webapp.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
