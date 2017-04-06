0daa20d603763d96b7bcdba8619c97ff06de5e2e
Merlijn <merlijn@familie-wissink.com>
2017-04-06 15:12:17 +0200

    Role authorization now sort of works

--- indicium_webapp/Models/ApplicationRole.cs
+++ indicium_webapp/Models/ApplicationRole.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace indicium_webapp.Data
+namespace indicium_webapp.Models
 {
     public class ApplicationRole : IdentityRole
     {
