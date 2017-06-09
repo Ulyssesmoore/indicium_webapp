5a1289a1456de118309f22be551dd7ca7c8e05cf
Merlijn <merlijn@familie-wissink.com>
2017-06-09 08:58:31 +0200

    Gegenereerde code en pagina's

---
+++ indicium_webapp/Controllers/ActivityTypesController.cs
@@ -0,0 +1,153 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using indicium_webapp.Data;
+using indicium_webapp.Models;
+
+namespace indicium_webapp.Controllers
+{
+    public class ActivityTypesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ActivityTypesController(ApplicationDbContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: ActivityTypes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ActivityType.ToListAsync());
+        }
+
+        // GET: ActivityTypes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var activityType = await _context.ActivityType
+                .SingleOrDefaultAsync(m => m.ActivityTypeID == id);
+            if (activityType == null)
+            {
+                return NotFound();
+            }
+
+            return View(activityType);
+        }
+
+        // GET: ActivityTypes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ActivityTypes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ActivityTypeID,Name,BackgroundColor,LineColor,TextColor")] ActivityType activityType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(activityType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(activityType);
+        }
+
+        // GET: ActivityTypes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var activityType = await _context.ActivityType.SingleOrDefaultAsync(m => m.ActivityTypeID == id);
+            if (activityType == null)
+            {
+                return NotFound();
+            }
+            return View(activityType);
+        }
+
+        // POST: ActivityTypes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ActivityTypeID,Name,BackgroundColor,LineColor,TextColor")] ActivityType activityType)
+        {
+            if (id != activityType.ActivityTypeID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(activityType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ActivityTypeExists(activityType.ActivityTypeID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(activityType);
+        }
+
+        // GET: ActivityTypes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var activityType = await _context.ActivityType
+                .SingleOrDefaultAsync(m => m.ActivityTypeID == id);
+            if (activityType == null)
+            {
+                return NotFound();
+            }
+
+            return View(activityType);
+        }
+
+        // POST: ActivityTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var activityType = await _context.ActivityType.SingleOrDefaultAsync(m => m.ActivityTypeID == id);
+            _context.ActivityType.Remove(activityType);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool ActivityTypeExists(int id)
+        {
+            return _context.ActivityType.Any(e => e.ActivityTypeID == id);
+        }
+    }
+}
