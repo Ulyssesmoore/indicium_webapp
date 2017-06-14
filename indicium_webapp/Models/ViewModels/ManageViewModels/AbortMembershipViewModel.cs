61eacc2657e4e874baad1abdc893a058f2cb69ce
Tegrohir <pim.bloot@student.hu.nl>
2017-06-14 19:07:47 +0200

    Final ManageUpdate -- All of the old unused code should be removed from ManageController, Views\Manage, Models\Viewmodels\ManageViewModels. -- Added support for aborting one's membership
    Bugfixes:
    -- Fixed a bug where upon visiting ChangePersonalInformation and submitting the form you'd get a Bad Request - Invalid URL

---
+++ indicium_webapp/Models/ViewModels/ManageViewModels/AbortMembershipViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace indicium_webapp.Models.ViewModels.ManageViewModels
+{
+    public class AbortMembershipViewModel
+    {
+        public string UserName { get; set; }
+    }
+}
