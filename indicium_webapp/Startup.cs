6f79708d6275818e18acea23a590deef37b46fa6
Sander de Bruin <sander.debruin@student.hu.nl>
2017-06-15 15:19:45 +0200

    Introducing Session variables

--- indicium_webapp/Startup.cs
+++ indicium_webapp/Startup.cs
@@ -86,7 +86,6 @@ namespace indicium_webapp
             }
 
             app.UseStaticFiles();
-
             app.UseIdentity();
 
             // Add external authentication middleware below. To configure them please see https://go.microsoft.com/fwlink/?LinkID=532715
