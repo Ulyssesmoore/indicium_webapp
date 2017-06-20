c3552adeb1432b3e5df1e7b23d34e59df32dcd10
Merlijn <merlijn@familie-wissink.com>
2017-06-20 16:27:26 +0200

    New age validation

---
+++ indicium_webapp/Data/ValidationAttributes/MinimumAgeAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace indicium_webapp.Data.ValidationAttributes
+{
+    public class MinimumAgeAttribute : ValidationAttribute
+    {
+        private readonly int _minimumAge;
+
+        public MinimumAgeAttribute(int minimumAge)
+        {
+            _minimumAge = minimumAge;
+        }
+
+        public override bool IsValid(object value)
+        {
+            DateTime date;
+            if (DateTime.TryParse(value.ToString(), out date))
+            {
+                return date.AddYears(_minimumAge) < DateTime.Now;
+            }
+
+            return false;
+        }
+    }
+}
--- indicium_webapp/Models/ViewModels/AccountViewModels/RegisterViewModel.cs
+++ indicium_webapp/Models/ViewModels/AccountViewModels/RegisterViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
+using indicium_webapp.Data.ValidationAttributes;
 using indicium_webapp.Models.InterfaceItemModels;
 
 namespace indicium_webapp.Models.ViewModels.AccountViewModels
@@ -47,6 +49,7 @@ namespace indicium_webapp.Models.ViewModels.AccountViewModels
         [Required(ErrorMessage = "{0} is verplicht.")]
         [StringLength(100, ErrorMessage = "{0} mag maximaal {1} karakter(s) zijn.")]
         [DataType(DataType.DateTime)]
+        [MinimumAge(16, ErrorMessage = "De minimumleeftijd is 16 jaar.")]
         [Display(Name = "Geboortedatum")]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd-MM-yyyy}")]
         public string Birthday { get; set; }
