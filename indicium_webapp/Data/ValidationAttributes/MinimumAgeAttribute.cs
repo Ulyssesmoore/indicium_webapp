d014b57a7852679d947f2f7afa134b0fe70bc583
Merlijn <merlijn@familie-wissink.com>
2017-06-21 21:43:01 +0200

    Improved minimum age validation

--- indicium_webapp/Data/ValidationAttributes/MinimumAgeAttribute.cs
+++ indicium_webapp/Data/ValidationAttributes/MinimumAgeAttribute.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,7 +19,7 @@ namespace indicium_webapp.Data.ValidationAttributes
         public override bool IsValid(object value)
         {
             DateTime date;
-            if (DateTime.TryParse(value.ToString(), out date))
+            if (DateTime.TryParseExact(value.ToString(), "dd-MM-yyyy", new CultureInfo("nl-NL"), DateTimeStyles.None, out date))
             {
                 return date.AddYears(_minimumAge) < DateTime.Now;
             }
